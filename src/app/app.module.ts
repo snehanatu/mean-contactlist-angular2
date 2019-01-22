e32729418663e8801dd1c0b1d93044f65a641054
monali mayaskar <snehanatu@yahoo.com>
2019-01-22 16:17:29 -0500

    pushing changes to server.js and whatnot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
-import { ServiceComponent } from './service/service.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactDetailsComponent,
     ContactListComponent,
-    ServiceComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
